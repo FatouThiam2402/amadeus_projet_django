83c830e522a3c3cdcad5e09da5cfa0c4876bbaf3
XpertE0! <dioufm65@yahoo.com>
2023-05-28 11:44:13 +0000

    Insertion base de donnees vols

--- vols/views.py
+++ vols/views.py
@@ -13,6 +13,17 @@ amadeus = Client(
 
 def index(request) :
  	return render(request, 'vols/choixvol.html')
+ 	
+ 	
+from django.db import connection
+
+def my_custom_sql(self):
+    with connection.cursor() as cursor:
+        cursor.execute("UPDATE bar SET foo = 1 WHERE baz = %s", [self.baz])
+        cursor.execute("SELECT foo FROM bar WHERE baz = %s", [self.baz])
+        row = cursor.fetchone()
+
+    return row
 
 
 def select_destination(req, param):
