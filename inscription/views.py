16c253bed2a9f29f8f39d695c20a0c2bf331061b
Moussa Diouf <79112858+JoofMoussa@users.noreply.github.com>
2023-06-09 09:10:34 +0000

    Add files via upload

--- inscription/views.py
+++ inscription/views.py
@@ -1,30 +1,52 @@
 from django.contrib import messages
-from django.shortcuts import render, redirect, get_object_or_404
-from django.views.generic import View
-# from hotel.models import Doctor
-from .models import Inscription
+from django.shortcuts import render
+import mysql.connector
 
 
-class AppointmentView(View):
-    def get(self, request, *args, **kwargs):
-        # context = {
-        #     'doctors': Doctor.objects.all()
-        # }
-        return render(request, "inscription/index.html")
 
-    def post(self, request, *args, **kwargs):
-        name = request.POST.get('name')
-        phone = request.POST.get('phone')
-        email = request.POST.get('email')
-        # doctor_id = request.POST.get('doctor')
-        date = request.POST.get('date')
-        time = request.POST.get('time')
-        note = request.POST.get('note')
-        # if doctor_id:
-        #     doctor = get_object_or_404(Doctor, id=doctor_id)
 
-        if(name and phone and email and date and time):
-            Appointment.objects.create(
-                name=name, phone=phone, email=email, date=date, time=time, note=note)
-            messages.success(request,'inscription Reussi')
-        return redirect('inscription')
+def post(request):
+            connection = mysql.connector.connect(
+                    host='localhost',
+                    user='Moussa',
+                    password='root123',
+                    database='amadeus'
+            )
+        
+            # cursor = connection.cursor()
+    # if request.method == 'POST':
+            name  = request.POST.get('nom')
+            phone = request.POST.get('phone')
+            email = request.POST.get('email')
+            adr   = request.POST.get('adresse')
+            date  = request.POST.get('date')
+            pass1  = request.POST.get('password1')
+            pass2  = request.POST.get('password2')
+            vide  = '2020-01-01'
+            with connection.cursor() as cursor:
+                checkid = "SELECT * FROM Client WHERE telephone= %s AND email = %s"
+                params = [phone, email]
+                cursor.execute(checkid, params)
+                results = cursor.fetchall()
+                # for res in results:
+                if (len(results)>0) :
+                        messages.error(request, 'Cet utilisateur existe deja dans la base de Donnee')
+                        return render(request, 'inscription/index.html')
+                else :
+                    if (pass1==pass2):
+                        messages.error(request, 'Les mots de passes deonnees ne sont pas les memes')
+                    query = """
+                                    INSERT INTO Client (nomComplet, telephone, email, adresse,dernierTicketAchete, date_naissance, password1, password2)
+                                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+                            """
+                cursor.execute(query, (name, phone, email, adr,vide, date, hash(pass1), hash(pass2) ))
+                results = cursor.fetchall()
+                context = {'results': results}
+                connection.commit()
+                connection.close()
+                messages.success(request,'inscription Reussi')
+                return render(request, 'inscription/index.html', context)
+
+
+def mesreservations(request):
+    return render(request, 'espaceClient/mesreservations.html')
