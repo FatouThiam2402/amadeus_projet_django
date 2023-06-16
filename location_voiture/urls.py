4160148c2f063f5d7e1860beec1b980a9c511a2c
salimata97 <s.ba20171238@zig.univ.sn>
2023-06-16 08:28:58 +0000

    ajout

--- location_voiture/urls.py
+++ location_voiture/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('html_voiture/', views.index, name='index'),
-     
+    path('bouton_reservation/', views.process_reservation, name='process_reservation'),
+    path('html_reservation/', views.process_recuper, name='process_recuper'),
 ]
+
 
--- location_voiture/views.py
+++ location_voiture/views.py
@@ -1,7 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.db import connection
+# import mysql.connector as sql
 
+# connection = sql.connect(
+# 	host='localhost',
+# 	user='Moussa',
+# 	password='root123',
+# 	database='amadeus'
+# )
 
 # Create your views here.
 
@@ -30,10 +37,65 @@ def index(request) :
 	
 
 	# reservation
-	def reservation(request,parametre):
-		pass
+def process_reservation(request):
+	if request.method == 'post':
+			field1_value = request.POST.get['pickup_location']
+			field2_value = request.POST.get['return_location']
+			field3_value = request.POST.get['vehicle_type']
+			field4_value = request.POST.get['passenger_count']
+			field5_value = request.POST.get['marque']
+			field6_value = request.POST.get['prix']
+			field7_value = request.POST.get['modele']
+			field8_value = request.POST.get['couleur']
+			field9_value = request.POST.get['additional_options']
+			mes_valeurs=[field3_value,field4_value,field5_value,field7_value,field8_value,field9_value]
+	
+			requette="""INSERT INTO Reservation (dateReservation,Date_retour,prixReservation,Reserve_voiture)
+			VALUES (field1_value,field2_value,field6_value,mes_valeurs)"""
+			with connection.cursor() as cursor:
+				cursor.execute(requette)
+			resultats = cursor.fetchall()
+			context = {'voitures' : resultats}
+			# Faites quelque chose avec les valeurs récupérées
 
+			return render(request, 'html_voiture.html', context)
+	else:
 		
+		return render(request, 'html_voiture.html')
+	
+def process_recuper(request):
+		# if request.method == 'post':
+			# field1_value = request.POST.get['pickup_location']
+			# field2_value = request.POST.get['return_location']
+			# field3_value = request.POST.get['vehicle_type']
+			field4_value = request.POST.get['passenger_count']
+			field5_value = request.POST.get['marque']
+			# field6_value = request.POST.get['prix']
+			# field7_value = request.POST.get['modele']
+			# field8_value = request.POST.get['couleur']
+			# field9_value = request.POST.get['additional_options']
+			# requette="""SELECT * FROM  Voiture WHERE nombre_places = %d OR marque = %s OR prix = %d OR modele = %s OR couleur = %s)
+			
+			# """
+			requette="""SELECT * FROM Voiture WHERE nombre_places=3"""
+			# parameters=[field3_value,field4_value,field5_value,field6_value,field7_value,field8_value]
+			parameters=[field4_value]
+			print(requette)
+			cursor = connection.cursor() 
+			cursor.execute(requette, parameters)	
+			results = cursor.fetchall()
+			print(results)	
+			# Faites quelque chose avec les valeurs récupérées
+
+			return render(request, 'location_voiture/html_voiture.html', {'voitures':results})
+	# else:
+		
+		# return render(request, 'location_voiture/html_voiture.html')
+	
+
+	
+
+	
 
 
 
